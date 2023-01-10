329dff054165b90172907a375bb0b3a7de923a0a
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-01-10 08:56:10 +0700

    change to lambda symbol
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- 3/main.rkt
+++ 3/main.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 (apply *
-       (filter (lambda (x) (= (modulo 600851475143 x) 0))
+       (filter (λ (x) (= (modulo 600851475143 x) 0))
                (range 1 30000)))
 
 (last
- (filter (lambda (x) (= (modulo 600851475143 x) 0))
+ (filter (λ (x) (= (modulo 600851475143 x) 0))
          (range 1 30000)))

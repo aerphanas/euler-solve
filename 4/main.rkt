a03dcacd89301d909d33bf84aa7131f39bf50742
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-01-10 09:44:37 +0700

    add solution
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ 4/main.rkt
@@ -0,0 +1,20 @@
+;./main | grep "its palidrome" | sort -n | tail -n 5
+#lang racket
+
+(require racket/list)
+
+(define (number-palidrome? i)
+  (define (string-palidrome? s)
+    (if (string=?
+         s
+         (list->string (reverse (string->list s))))
+        #t
+        #f))
+  (if (string-palidrome? (number->string i)) #t #f))
+
+(define check
+  (for ([x (range 100 1000)])
+    (for ([y (range 100 1000)])
+      (if (number-palidrome? (* x y))
+          (printf "~a from ~a * ~a : its palidrome\n"(* x y) x y)
+          (printf "~a from ~a * ~a : its not palidrome\n"(* x y) x y)))))
